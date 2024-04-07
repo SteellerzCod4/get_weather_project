ad854c3668465f01bac1d9cf1b8421cff0f3ef58
SteellerzCod4 <ilyastyllerz1996@yandex.ru>
2024-04-07 22:16:59 +0300

    correct code

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--boolargument', default=False, help='флаг включения чего-то', type=bool)
-parser.parse_args()
-print(parser.boolargument)
+args = parser.parse_args()
+print(args.boolargument)
