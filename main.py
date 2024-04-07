a7bc0f81c1874f42e3e36487197f1025c0b7f7cd
SteellerzCod4 <ilyastyllerz1996@yandex.ru>
2024-04-07 22:11:54 +0300

    correct type

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--boolargument', '-B', help='флаг включения чего-то')
+parser.add_argument('--boolargument', '-B', help='флаг включения чего-то', type=bool)
 parser.parse_args()
 print(parser.boolargument)
